459f84adca06bb6ae97a5f7339aec84a1f76986d
Giselle Pacheco <gisellepacheco@Giselles-MacBook-Pro.local>
2019-12-01 09:41:16 -0500

    Day 1: part 1 and 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent Of Code 2019
+
+Solutions for advent of code 2019 problems
---
+++ src/AdventOfCode2019_02.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class AdventOfCode2019_02 {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
